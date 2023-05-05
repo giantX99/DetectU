a1d5c56f54e6132e88ac693a17dcfcb0e718b3d7
giantX99 <93807005+giantX99@users.noreply.github.com>
2023-05-04 19:07:33 -0500

    Delete Qt_test directory

--- Qt_test/build-test-Desktop_Qt_6_5_0_MinGW_64_bit-Debug/qtcsettings.cmake
+++
@@ -1,3 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("OpenCV_DIR" ""C:/Users/gian-/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/opencv/build/x64/vc16/lib"" CACHE "PATH" "" FORCE)
